996c6386b925de0452c736921ec100a6109e470c
Yicun Hou <yicunhou@bu.edu>
2020-06-10 12:58:33 -0400

    Add select and load player function

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,34 +4,43 @@ import nba from 'nba';
 import Profile from './Profile';
 import DataViewContainer from './DataViewContainer';
 import SearchBar from './SearchBar'
+
+import { DEFAULT_PLAYER_INFO } from '../constants';
+
 class Main extends Component {
-   state = {
-       playerId: 201939,
-       playerInfo: {}
-   }
+  state = {
+    playerInfo: DEFAULT_PLAYER_INFO
+  }
 
-   componentDidMount() {
-       window.nba = nba;
-       nba.stats.playerInfo({ PlayerID: nba.findPlayer('Stephen Curry').playerId})
-           .then((info) => {
-               console.log(info);
-               const playInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
-               console.log(playInfo);
-               this.setState({ playerInfo: playInfo });
-           })
-   }
+  componentDidMount() {
+    window.nba = nba;
+    this.loadPlayerInfo('Stephen Curry');
+  }
 
-   render() {
-       return (
-           <div className="main">
-               <SearchBar />
-               <div className="player">
-                   <Profile playerInfo={this.state.playerInfo} />
-                   <DataViewContainer playerId={this.state.playerId}/>
-               </div>
-           </div>
-       );
-   }
+  loadPlayerInfo = (playerName) => {
+    nba.stats.playerInfo({ PlayerID: nba.findPlayer(playerName).playerId}).then((info) => {
+      console.log(info);
+      const playInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
+      console.log(playInfo);
+      this.setState({ playerInfo: playInfo });
+    })
+  }
+
+  handleSelectPlayer = (playerName) => {
+    this.loadPlayerInfo(playerName);
+  }
+
+  render() {
+    return (
+      <div className="main">
+        <SearchBar handleSelectPlayer={this.handleSelectPlayer}/>
+        <div className="player">
+          <Profile playerInfo={this.state.playerInfo} />
+          <DataViewContainer playerId={this.state.playerInfo.playerId}/>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Main;
