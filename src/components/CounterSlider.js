0e8b34e3b12d3a809435f91dccf492bfa06f471a
Yicun Hou <yicunhou@bu.edu>
2020-06-10 10:21:28 -0400

    Make counterSlider response dynamically.

--- src/components/CounterSlider.js
+++ src/components/CounterSlider.js
@@ -2,41 +2,36 @@ import React from 'react';
 import { Slider, InputNumber, Row, Col } from 'antd';
 
 class CounterSlider extends React.Component {
-  state = {
-    inputValue: 1,
-  };
+ state = {
+    inputValue: this.props.value
+ }
 
-  onChange = value => {
-    this.setState({
-      inputValue: value,
-    });
-    this.props.handleCountSliderChange(value);
-  };
+ onChange = (value) => {
+   const cleanValue = Number(value) ? value: this.state.inputValue;
+   this.setState({
+     inputValue: cleanValue,
+   });
+   this.props.onCountSliderChange(cleanValue);
+ }
 
-  render() {
-    const { inputValue } = this.state;
-    return (
-      <Row>
-        <Col span={12}>
-          <Slider
-            min={1}
-            max={20}
-            onChange={this.onChange}
-            value={typeof inputValue === 'number' ? inputValue : 0}
-          />
-        </Col>
-        <Col span={4}>
-          <InputNumber
-            min={1}
-            max={20}
-            style={{ margin: '0 16px' }}
-            value={inputValue}
-            onChange={this.onChange}
-          />
-        </Col>
-      </Row>
-    );
-  }
+ render() {
+   return (
+     <Row>
+       <Col span={12}>
+         <Slider min={1} max={20} onChange={this.onChange} value={this.state.inputValue} />
+       </Col>
+       <Col span={4}>
+         <InputNumber
+           min={1}
+           max={20}
+           style={{ marginLeft: 16 }}
+           value={this.state.inputValue}
+           onChange={this.onChange}
+         />
+       </Col>
+     </Row>
+   );
+ }
 }
 
 export default CounterSlider;
